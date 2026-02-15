56c16a9295a663b2cb52eebf2620c488a2bbda54
unknown <vinicio.barreto.santos@gmail.com>
2026-02-14 21:56:15 -0300

    test 21342

--- remotes/home/src/app.jsx
+++ remotes/home/src/app.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 const _ = React; 
 
-const App = () => (<p className="home">Home Page</p>);
+const App = () => (<p>Home Page</p>);
 
 export default App;
--- remotes/list-movies/src/app.jsx
+++ remotes/list-movies/src/app.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 const _ = React; 
 
-const App = () => (<h1 className="listw">Imagine movies being listed here</h1>);
+const App = () => (<h1>Imagine movies being listed here</h1>);
 
 export default App;
