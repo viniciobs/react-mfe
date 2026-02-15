c7e4d9e269a094138499977ef88a5efa293e15d1
unknown <vinicio.barreto.santos@gmail.com>
2026-02-14 23:30:42 -0300

    test 23213126361283812d32342351384231321143213

--- remotes/list-movies/src/app.jsx
+++ remotes/list-movies/src/app.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 const _ = React; 
 
-const App = () => (<h2>Imagine movies being listed here</h2>);
+const App = () => (<h1>Imagine movies being listed here</h1>);
 
 export default App;
