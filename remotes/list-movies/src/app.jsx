32ed3a01cc9bbee25f76b43334bf9367bd38e11e
unknown <vinicio.barreto.santos@gmail.com>
2026-02-14 21:35:23 -0300

    test 213412312

--- remotes/list-movies/src/app.jsx
+++ remotes/list-movies/src/app.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 const _ = React; 
 
-const App = () => (<h1 className="list-movies">Imagine movies being listed here</h1>);
+const App = () => (<h1 className="listmovies">Imagine movies being listed here</h1>);
 
 export default App;
