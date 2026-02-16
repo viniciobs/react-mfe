9e344e26607f4c7bf35b38262c2b1b9adfd1fac6
unknown <vinicio.barreto.santos@gmail.com>
2026-02-16 02:07:14 -0300

    test deployment

--- remotes/view-movie/src/app.jsx
+++ remotes/view-movie/src/app.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 const _ = React; 
 
-const App = () => (<h1>Imagine que tem um view movie aqui</h1>);
+const App = () => (<h2>Imagine que tem um view movie aqui</h2>);
 
 export default App;
